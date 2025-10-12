d405234ce108dcdd37192298aceb327efd4ed636
luizmasella <phkqmvhjtz@privaterelay.appleid.com>
2025-10-11 21:10:48 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
 // FILE: src/main.tsx
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-// 1. Importe o nosso novo Provedor de Contexto
-import { PericiasProvider } from "./context/PericiasContext.tsx";
+// Importamos nosso novo Provedor de Contexto
+import { PericiasProvider } from './context/PericiasContext.tsx';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    {/* 2. Envolva o <App /> com o <PericiasProvider> */}
+    {/* Envolvemos o <App /> com o <PericiasProvider> */}
     <PericiasProvider>
       <App />
     </PericiasProvider>
