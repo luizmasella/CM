882c70d4d8b90f066e3971b6e9e18ecfb353fae3
luizmasella <phkqmvhjtz@privaterelay.appleid.com>
2025-10-11 21:50:59 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
-// FILE: src/main.tsx
+// FILE: src/main.tsx (VERSÃO ATUALIZADA)
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
-import './index.css'; // Linha crucial para carregar os estilos
+import './index.css';
 
 import { PericiasProvider } from './context/PericiasContext.tsx';
+import { UIProvider } from './context/UIContext.tsx'; // <-- 1. Importamos o novo provider
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <PericiasProvider>
-      <App />
-    </PericiasProvider>
+    {/* 2. Aninhamos os providers. A ordem aqui não importa muito. */}
+    <UIProvider>
+      <PericiasProvider>
+        <App />
+      </PericiasProvider>
+    </UIProvider>
   </React.StrictMode>
 );
