96522a22df433af0629821fc08a29336cdc82ebc
luizmasella <phkqmvhjtz@privaterelay.appleid.com>
2025-10-11 22:51:09 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,20 @@
+// FILE: src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { PericiasProvider } from './context/PericiasContext.tsx';
 import { UIProvider } from './context/UIContext.tsx';
+import { ToastProvider } from './context/ToastContext.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <UIProvider>
-      <PericiasProvider>
-        <App />
-      </PericiasProvider>
-    </UIProvider>
+    <ToastProvider>
+      <UIProvider>
+        <PericiasProvider>
+          <App />
+        </PericiasProvider>
+      </UIProvider>
+    </ToastProvider>
   </React.StrictMode>
 );
